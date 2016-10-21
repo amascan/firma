add1926f3d53287e831c6088bbb3644b0180b54f
Adrian Mascan <adrianmascan@yahoo.com>
2016-10-21 17:38:11 +0300

    cxc

--- src/MainF.java
+++ src/MainF.java
@@ -10,7 +10,7 @@ public class MainF {
     public static void main(String[] args) {
         List<String> firme = new ArrayList<>();
         firme = Fileread.read("venituri.txt");
-         for(int i=1;i<firme.size();i++){
+        for(int i=1;i<firme.size();i++){
             Venituri line = new Venituri(firme.get(i));
             line.calculare();
         }
---
+++ src/Venituri.java
@@ -0,0 +1,74 @@
+/**
+ * Created by Adrian on 19.10.2016.
+ */
+public class Venituri {
+    String numeF;
+    double trim1;
+    double trim2;
+    double trim3;
+    double trim4;
+    double venittotal = 0;
+    double venitmediu = 0;
+    double venitminim = 0;
+    double venitmaxim = 0;
+    double[] doublesplit2;
+
+
+    public Venituri(String linie) {
+        String[] split1 = linie.split(" ;   ");
+        numeF = split1[0].replaceAll("\\s+$", "");
+        String[] split2 = split1[1].split("        ; ");
+        doublesplit2 = new double[split2.length];
+        for (int i = 0; i < split2.length; i++) doublesplit2[i] = Double.valueOf(split2[i]);
+        trim1 = doublesplit2[0];
+        trim2 = doublesplit2[1];
+        trim3 = doublesplit2[2];
+        trim4 = doublesplit2[3];
+    }
+
+    public void calculare() {
+
+        venitmaxim = doublesplit2[0];
+        for (int i = 0; i < doublesplit2.length; i++) {
+            if (doublesplit2[i] > venitmaxim) {
+                venitmaxim = doublesplit2[i];
+            }
+        }
+
+        venitminim = doublesplit2[0];
+        for (int i = 0; i < doublesplit2.length; i++) {
+            if (doublesplit2[i] < venitminim) {
+                venitminim = doublesplit2[i];
+            }
+        }
+
+        venittotal = trim1 + trim2 + trim3 + trim4;
+        venitmediu = venittotal / 4;
+
+        System.out.println("Firma " + numeF);
+
+        System.out.print("Venit total: " + venittotal);
+        System.out.print("\t\tVenit trimestru1: " + trim1);
+        System.out.println("\t\tImpozit trim1: " + calculimpozit(trim1));
+        System.out.print("Venit mediu: " + venitmediu);
+        System.out.print("\t\tVenit trimestru2: " + trim2);
+        System.out.println("\t\tImpozit trim2: " + calculimpozit(trim2));
+        System.out.print("Venit minim: " + venitminim);
+        System.out.print("\t\tVenit trimestru3: " + trim3);
+        System.out.println("\t\tImpozit trim3: " + calculimpozit(trim3));
+        System.out.print("Venit maxim: " + venitmaxim);
+        System.out.print("\t\tVenit trimestru4: " + trim4);
+        System.out.println("\t\tImpozit trim4: " + calculimpozit(trim4));
+        System.out.println("-----------------------------------------------------------------------");
+
+    }
+
+    public double calculimpozit(double valoare) {
+        if (valoare < 15.5) {
+            return 15 * valoare / 100;
+        }
+        else {
+            return 20 * valoare / 100;
+        }
+    }
+}
